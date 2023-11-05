82a47ebbd76c048cdc4fcdd7c365830358a52e0a
HLCaptain <pkblazsak@gmail.com>
2023-11-05 23:05:56 +0100

    Removed unused code.

--- planty-client/common/src/jsMain/kotlin/nest/planty/platform.kt
+++ planty-client/common/src/jsMain/kotlin/nest/planty/platform.kt
@@ -1,14 +1,7 @@
 package nest.planty
 
-import androidx.compose.runtime.Composable
-
 actual fun getPlatformName(): String {
     return "Web"
 }
 
 actual interface JavaSerializable
-
-@Composable
-fun UIShow() {
-    App()
-}
